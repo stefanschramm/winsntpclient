9e6b89be97f1780df1a14a88defd513c31639b18
Stefan Schramm <mail@stefanschramm.net>
2025-04-17 08:19:36 +0200

    Clean up resource.h

--- resource.h
+++ resource.h
@@ -2,23 +2,14 @@
 // Microsoft Developer Studio generated include file.
 // Used by winsntpclient.rc
 //
-#define IDC_MYICON                      2
-#define IDD_WINSNTPCLIENT_DIALOG        102
-#define IDD_ABOUTBOX                    103
-#define IDS_APP_TITLE                   103
-#define IDM_ABOUT                       104
-#define IDM_EXIT                        105
-#define IDS_HELLO                       106
 #define IDI_WINSNTPCLIENT               107
 #define IDI_SMALL                       108
-#define IDC_WINSNTPCLIENT               109
-#define IDR_MAINFRAME                   128
-#define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
